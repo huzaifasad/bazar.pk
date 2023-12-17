70dce0e2b2092f69c52625df7f94696a7f47296f
Huzaifa Saad <140094374+huzaifasad@users.noreply.github.com>
2023-12-17 10:28:58 -0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,15 +1,34 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-require('./schemas/config');
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-app.use(express.json());
-app.use(cors());
+const url =
+  "mongodb+srv://huzaifa084567:12345@cluster0.wpihnwn.mongodb.net/your-database-name?retryWrites=true&w=majority";
+mongoose
+  .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("connected to the database"))
+  .catch(() => console.log("not conncted"));
 
-const laptoprouter = require('./schemas/productrouter/laptop');
-const userrouter=require('./schemas/productrouter/user')
-app.use('/laptop', laptoprouter);
-app.use('/user',userrouter);
 app.listen(1000, () => {
-  console.log('Server is running on port 1000');
+  console.log("Server is running on port 1000");
+});
+
+app.use(
+  cors({
+    origin: ["https://backendbazar-pk.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
+const laptoprouter = require("./schemas/productrouter/laptop");
+const userrouter = require("./schemas/productrouter/user");
+app.use(bodyParser.json({ extended: true }));
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/laptop", laptoprouter);
+app.use("/user", userrouter);
+app.use("/", (req, res) => {
+  res.send("thiis response");
 });
